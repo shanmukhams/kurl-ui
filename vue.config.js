3da9c9f240783346178c3f49dd27654378124643
shanmukha <shanmukha.krish123@gmail.com>
2021-11-30 02:24:44 +0100

    prod

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'https://kurl-server.herokuapp.com/'
+    : 'localhost:3000/'
 }
